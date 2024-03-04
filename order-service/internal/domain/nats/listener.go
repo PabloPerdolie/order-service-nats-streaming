f5f5ae349a4e38098700bc5bf4ef7428e4a6eb32
Pablo <parusov.pashka@ya.ru>
2024-03-05 00:34:10 +0300

    nats setting ended

--- order-service/internal/domain/nats/listener.go
+++ order-service/internal/domain/nats/listener.go
@@ -1,6 +1,7 @@
 package nats
 
 import (
+	"L0/order-service/internal/domain/config"
 	"L0/order-service/internal/domain/models"
 	"L0/order-service/internal/domain/postgres"
 	"context"
@@ -10,25 +11,37 @@ import (
 )
 
 func InitListener() error {
-
-	natsCon, err := nats.Connect("nats://localhost:4222")
+	conf := config.CONFIG.Nats
+	natsCon, err := nats.Connect(conf.URL)
 	if err != nil {
-		log.Fatalf("failed to connect nats: %v", err)
+		return err
+	}
+
+	js, err := natsCon.JetStream()
+
+	if err != nil {
 		return err
 	}
 
 	log.Println("Successfully initialized nats-listener")
 
-	_, err = natsCon.Subscribe("test.orders", func(msg *nats.Msg) {
-		log.Printf("mes received %v", msg.Data)
+	_, err = js.Subscribe(conf.SubjectName, func(msg *nats.Msg) {
+		msg.Ack()
+
 		var order models.Order
 		err := json.Unmarshal(msg.Data, &order)
 		if err != nil {
 			return
 		}
+		log.Printf("mes received %v", order)
 		postgres.CreateOrder(context.Background(), order)
 		//todo CACHE
-	})
+	}, nats.Durable(conf.Subscriber), nats.ManualAck())
+
+	if err != nil {
+		return err
+	}
+
 	if err != nil {
 		return err
 	}
